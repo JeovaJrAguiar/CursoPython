ca96f9c8801ec1c3c7731931af22a1a469e8f80c
Aguiar <jeova.junior897@gmail.com>
2022-03-18 23:41:13 -0300

    .

---
+++ Exercicios/ex005.py
@@ -0,0 +1,4 @@
+# Faca um programa que leia na tela um numero e mostra seu sucessor e seu antecessor
+num = int(input("Num: "))
+
+print("O antecessor de {} eh: {} e seu sucessor eh: {}".format(num, num-1, num+1))
