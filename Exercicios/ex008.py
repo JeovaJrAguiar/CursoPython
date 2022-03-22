15fa20c14f8078d50bb0aa2cff11affb82fdc444
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2022-03-22 16:50:45 -0300

    update in file

--- Exercicios/ex008.py
+++ Exercicios/ex008.py
@@ -1,6 +1,6 @@
 # Crie um algoritimo que leia um valor em metros e o exiba convertido em centimetros e milimetros.
 # km hm dam m dm cm mm
-medida = float('distancia em metros: ')
+medida = float(input('Distancia em metros: '))
 
 km = medida / 1000
 hm = medida / 100
@@ -9,4 +9,5 @@ dm = medida * 10
 cm = medida * 100
 mm = medida * 1000
 
-print(''.format(mm, cm, dm, dam))
+#print('Km: {:.4f}'.format(km))
+print('Km: {:.4f}\nHm: {:.4f}\nDam: {:.4f}\nDm: {:.4f}\nCm: {:.4f}\nMm: {:.4f}'.format(km, hm, dam, dm, cm, mm))
