b3e9c551deceb2434800abd58d0d1adcf5943a27
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2022-03-22 14:41:37 -0300

    create ex007.py

---
+++ Exercicios/ex007.py
@@ -0,0 +1,5 @@
+# Crie um algoritmo que leia dua snotas de um aluno, calcule e mostre sua media
+n1 = float(input('Nota 1: '))
+n2 = float(input('Nota 2: '))
+
+print('Media {:.2f}'.format((n1+n2)/2))
