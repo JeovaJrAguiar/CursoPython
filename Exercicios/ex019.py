4700249189ded01cacf4c297866291ba57501d95
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2022-04-05 11:01:04 -0300

    create 019

---
+++ Exercicios/ex019.py
@@ -0,0 +1,11 @@
+# Receba quatro nomes e sortei um dentre eles
+import random
+
+n1 = str(input('Primeiro aluno: '))
+n2 = str(input('Primeiro aluno: '))
+n3 = str(input('Primeiro aluno: '))
+n4 = str(input('Primeiro aluno: '))
+
+lista = [n1, n2, n3, n4]
+escolhido = random.choice(lista)
+print('Escolhido: {}'.format(escolhido))
