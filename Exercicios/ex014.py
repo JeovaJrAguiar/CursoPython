f1fb1573eecd4f21adbd69ad3720888b4a01f813
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2022-04-05 10:26:19 -0300

    create ex014 in pyhton

---
+++ Exercicios/ex014.py
@@ -0,0 +1,4 @@
+# Escreva um programa que recebe uma temperatura em ºC e transforma em ºF
+c = float(input('Temperatura em ºC: '))
+
+print('A temperatura de {:.2f}ºC é {:.2f}ºF'.format(c, (9*c)/5+32) )
