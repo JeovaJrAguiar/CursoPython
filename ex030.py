8e1d049da161bd0a72987503ce67c002d1609e55
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2022-04-11 10:05:22 -0300

    create file ex030 in python

---
+++ ex030.py
@@ -0,0 +1,8 @@
+# Crie um programa que leia um numero inteiro e mostre na tela se ele é par ou impar
+
+num = int(input('Digite um numero qualquer: '))
+
+if (num % 2) == 1:
+    print('Esse numero eh impar!')
+else:
+    print('Esse numero eh par!')
