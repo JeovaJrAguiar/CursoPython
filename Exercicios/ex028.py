c95e39d11226bbe27fb688bcec92bb3af21c52c8
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2022-04-11 09:41:27 -0300

    .

---
+++ Exercicios/ex028.py
@@ -0,0 +1,7 @@
+# Faca um programa que faca o sorteio de um numero inteiro entre 0 e 5
+
+from random import randint
+
+computador = randint(0,5)
+
+print('Numero sorteado {}'.format(computador))# faz o sorteio
