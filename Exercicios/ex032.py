935bc4b22c0dc21de29ae7d1062566d6a564bcc6
JeovaJrAguiar <jeova.junior897@gmail.com>
2022-11-07 23:44:29 -0300

    .

--- Exercicios/ex032.py
+++ Exercicios/ex032.py
@@ -1,3 +1,13 @@
-#
+# Desenvolva um programa que leia um ano qualquer e mostre se ele é bissexto
+from datetime import date
 
-ano = int(input('Qual ano quer analisar: '))
+ano = int(input('Qual ano quer analisar(coloque 0 para ano atual): '))
+
+
+if ano == 0:
+    ano = date.today().year
+
+if ano%4  == 0 and ano%100 != 0 or ano%400 == 0:
+    print('O ano {} é BISSEXTO'.format(ano))
+else:
+    print('O ano {} nao é BISSEXTO'.format(ano))
